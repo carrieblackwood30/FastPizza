e721d00b8881e10a98e9080bfcb52245b3b2f0eb
Meirzhan <carrieblackwood30@gmail.com>
2024-04-23 06:47:41 +0500

    add classes

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,12 +1,12 @@
 <template>
 
     <nav>
-        <div class="nav-container">
-            <button>все</button>
-            <button>мясные</button>
-            <button>Вегетарианская</button>
-            <button>Гриль</button>
-            <button>Острые</button>
+        <div class="nav-container" v-for="pizzaFilter in pizzas" :key="pizzaFilter">
+            <button :class="`pizza-filter-${pizzaFilter.class === 'all' ? 'on' : 'off'}`" @click="pizzaFilter.class = 'all'; pizzaFilter = 'all'">все</button>
+            <button :class="`pizza-filter-${pizzaFilter.class === 'meat' ? 'on' : 'off'}`" @click="pizzaFilter.class = 'meat'">мясные</button>
+            <button :class="`pizza-filter-${pizzaFilter.class === 'vegan' ? 'on' : 'off'}`" @click="pizzaFilter.class = 'vegan'">Вегетарианская</button>
+            <button :class="`pizza-filter-${pizzaFilter.class === 'grill' ? 'on' : 'off'}`" @click="pizzaFilter.class = 'grill'">Гриль</button>
+            <button :class="`pizza-filter-${pizzaFilter.class === 'spice' ? 'on' : 'off'}`" @click="pizzaFilter.class = 'spice'">Острые</button>
             <button>Закрытые</button>
         </div>
 
@@ -23,21 +23,27 @@
     <main>
         <h3>Все пиццы</h3>
         <div class="pizza-container">
-            <div class="pizza-card" v-for="pizza in pizzas[0]" :key="pizza">
+            <div class="pizza-card" v-for="(pizza) in pizzas[0]" :key="pizza" v-show="pizza.class.includes('vegan')"> <!--v-show-->
                 <img :src="pizza.img" alt="pizza-img">
                 <h3 style="text-align: center; margin: .8rem 1rem;">{{ pizza.name }}</h3>
                 <div class="button-container">
                     <div class="thickness">
-                    <button>тонкое</button>
-                    <button>традиционное</button>
+                        <button :class="`display-${pizza.thickness ? 'off' : 'on'}`"
+                            @click="pizza.thickness = false">тонкое</button>
+                        <button :class="`display-${pizza.thickness ? 'on' : 'off'}`"
+                            @click="pizza.thickness = true">традиционное</button>
+                    </div>
+                    <div class="width">
+                        <button :class="`display-${pizza.width === 26 ? 'on' : 'off'}`" @click="pizza.width = 26">26см.</button>
+                        <button :class="`display-${pizza.width === 30 ? 'on' : 'off'}`" @click="pizza.width = 30">30см.</button>
+                        <button :class="`display-${pizza.width === 40 ? 'on' : 'off'}`" @click="pizza.width = 40">40см.</button>
+                    </div>
                 </div>
-                <div class="width">
-                    <button>26 см.</button>
-                    <button>30 см.</button>
-                    <button>40 см.</button>
+                <div class="total-container">
+                    <h3 style="display: inline;">от {{ pizza.cost[0] }} ₽</h3>
+                    <button style="display: inline;" @click="totalPizza(pizza)">+ Добавить {{
+                        pizza.cost[prisePicker(pizza)]}}</button>
                 </div>
-                </div>
-                <h3 style="display: inline;">{{ pizza.cost }}<button style="display: inline;">+ Добавить 'cena'</button></h3>
             </div>
         </div>
     </main>
@@ -45,63 +51,73 @@
 </template>
 
 <script setup>
-    import { useStore } from "@/stores/dataBase.js"
-    import axios from "axios";
-    import { ref } from "vue";
+import { useStore } from "@/stores/dataBase.js"
+import axios from "axios";
+import { ref } from "vue";
 
-    const pizzas = ref([])
+const pizzas = ref([])
+const pizzaFilter = ref()
+console.log(pizzaFilter.value)
 
-    axios.get("http://localhost:3000/pizzas")
-    .then((response) =>{
+function prisePicker(pizza) {
+    if (pizza.width === 40) {
+        return 2
+    } else if (pizza.width === 30) {
+        return 1
+    } else return 0
+}
+
+axios.get("http://localhost:3000/pizzas")
+    .then((response) => {
         pizzas.value.push(response.data)
-        console.log(pizzas.value[0])
+        console.log(pizzas.value[0][0].class.includes('all'))
     })
 
 
 </script>
 
 <style scoped>
-
-nav{
+nav {
     width: 100%;
     justify-content: center;
     display: flex;
     gap: 20rem;
 }
 
-.nav-container{
+.nav-container {
     display: flex;
     gap: 4rem;
 }
 
-.nav-container button{
+.nav-container button {
     font-size: 16px;
-    background: transparent;
     font-weight: 700;
+    border-radius: 20px;
+    padding: 6px 12px;
 }
 
-label{
+label {
     font-size: 14px;
     font-weight: 700;
 }
 
-select{
+select {
     border: none;
     margin-left: 1rem;
     color: var(--main-color);
 }
 
-option{
+option {
     color: black;
 }
 
-main{
+main {
     margin: 2rem;
     width: 80%;
     margin-left: 14%;
 }
 
-.pizza-container{
+.pizza-container {
     margin: 1rem;
     display: flex;
     flex-wrap: wrap;
@@ -109,19 +125,19 @@ main{
     gap: 4rem;
 }
 
-.pizza-card{
+.pizza-card {
     width: 280px;
     display: flex;
     flex-direction: column;
 }
 
-.button-container{
+.button-container {
     background-color: #F3F3F3;
-    padding: .4rem;
+    padding: .2rem;
     border-radius: 10px;
 }
 
-.button-container button{
+.button-container button {
     font-size: 14px;
     font-weight: 700;
     width: 132px;
@@ -130,18 +146,47 @@ main{
     border-radius: 10px;
 }
 
-.button-container .thickness{
+.button-container .thickness {
     display: flex;
     justify-content: space-between;
 }
 
-.button-container .width{
+.button-container .width {
     display: flex;
     justify-content: space-between;
 }
 
-.button-container .width button{
+.button-container .width button {
     width: 80px;
 }
 
+.display-on {
+    background-color: #fff;
+}
+
+.total-container {
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem;
+}
+
+.total-container button {
+    border: 2px solid var(--main-color);
+    background-color: #F3F3F3;
+    color: var(--main-color);
+    border-radius: 20px;
+    padding: 6px 12px;
+    font-size: 16px;
+    font-weight: 700;
+}
+
+.total-container button:hover {
+    background-color: var(--main-color);
+    color: #F3F3F3;
+}
+
+.pizza-filter-on {
+    color: #F3F3F3;
+    background-color: #282828;
+}
 </style>
